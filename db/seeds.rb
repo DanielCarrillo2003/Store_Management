84ed636d52179e50d3f6c08830d4fa0ce242cabc
Daniel <daniel_carrillo_2003@outlook.com>
2023-12-10 03:00:10 -0600

    Sign up style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(username: "admin", email: 'admin@example.com', password: '12345678a', password_confirmation: '12345678a') if Rails.env.development?
